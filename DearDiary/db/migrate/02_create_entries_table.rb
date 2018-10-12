3f30156e11b11b3a3640730cdb8c82b21003a620
Madeline Stark <madeline.stark1@gmail.com>
2018-10-12 23:05:55 +0000

    Add date to entries table

--- DearDiary/db/migrate/02_create_entries_table.rb
+++ DearDiary/db/migrate/02_create_entries_table.rb
@@ -2,6 +2,7 @@ class CreateEntriesTable < ActiveRecord::Migration
   def change
     create_table :entries do |t|
       t.string :content
+      t.string :date
       t.integer :user_id
     end
   end
